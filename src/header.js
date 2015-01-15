d6f2e41b8a58f1346a63270f7c73585a0fff851e
Jochen Hinrichsen <jochen.hinrichsen@db.com>
2015-01-15 16:34:05 +0000

    Adding lead

--- src/header.js
+++ src/header.js
@@ -3,11 +3,35 @@
 
 "use strict"
 
-// Return default lead
+var assert = require('assert')
+
+// Return default lead buffer
 function lead() {
-    var buf = ne Buffer(96)
+    var leadSize = 96
+    var buf = new Buffer(leadSize)
+    buf.fill(0)
+
+    // magic
     buf[0] = 0xED
     buf[1] = 0xAB
     buf[2] = 0xEE
     buf[3] = 0xDB
+
+    // major
+    buf[4] = 3
+
+    // minor
+    buf[5] = 0
+
+    // type
+    buf[6] = 0
+    buf[7] = 0
+    buf[8] = 0
+    buf[9] = 0
+
+    return buf
 }
+
+
+var l1 = lead()
+assert.equal(l1.slice(0, 3) == [ 0xED, 0xAB, 0xEE, 0xDB ])
