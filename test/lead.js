9dba68dcdba64a9f76cd45b9329ee5a9c15f3037
Jochen Hinrichsen <jochen.hinrichsen@db.com>
2015-01-17 16:18:13 +0000

    Adding test case

--- test/lead.js
+++ test/lead.js
@@ -1,6 +1,22 @@
-var rpm = require('../')
+var rpm = require('../');
+var fs = require('fs');
 
 header = require('header');
+
+// Read existing rpm from local filesystem
+function readRpmLead(filename) {
+    fs.open(filename, 'r', function(status, fd) {
+        if (status) {
+            console.log(status.message);
+            return;
+        }
+        let buffer = new Buffer(100);
+        fs.read(fd, buffer, 0, 100, 0, function(err, num) {
+            console.log(buffer.toString('urf-8', 0, num));
+            return buffer;
+        });
+    });
+}
 
 module.exports = function(test) {
 
@@ -12,4 +28,10 @@ module.exports = function(test) {
 
   // TODO test lead with bad length -> throw
   // TODO test lead with bad magic -> throw
+
+  test('Read lead from rpm package', function(t) {
+      t.plan(2);
+      var l = readRpmLead('resources/s1.f1-1.0.0.0-1.x64_64.rpm');
+      console.log(lead.readLead(l));
+  });
 }
