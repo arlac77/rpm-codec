635b735bb68aae1e358c73fbbedd2b39a15f08ad
Markus Felten <Markus.Felten@gmx.de>
2015-01-31 00:23:28 +0100

    simple header test

--- test/extract.js
+++ test/extract.js
@@ -2,22 +2,16 @@ var rpm_stream = require('../lib/stream');
 var fs = require('fs');
 var path = require('path');
 
-QUnit.test('simple unpack', function(assert) {
-  console.log("assert: " + JSON.stringify(assert));
-  var done = assert.async();
-  var stream = rpm_stream();
-  expect(1);
+test('simple unpack', function() {
+	var stream = rpm_stream();
+	expect(1);
 
-  stream.on('header', function(header) {
-    equal(1, 1, "is good");
-    done();
-  });
+	stop();
+	stream.on('header', function(header) {
+		equal(1, 1, "is good");
+		start();
+	});
 
-  stream.on('error', function(error) {
-    console.log("error");
-    fail("error");
-  });
-
-  fs.createReadStream(path.join(__dirname,
-    'fixtures/mktemp-1.6-4mdv2010.1.i586.rpm')).pipe(stream);
+	fs.createReadStream(path.join(__dirname,
+		'fixtures/mktemp-1.6-4mdv2010.1.i586.rpm')).pipe(stream);
 });
