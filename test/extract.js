c232e541a0d74a04c62367ee7617c62671db8d4c
Markus Felten <Markus.Felten@gmx.de>
2015-01-31 00:28:36 +0100

    test failure of invalid rpm header

--- test/extract.js
+++ test/extract.js
@@ -2,16 +2,30 @@ var rpm_stream = require('../lib/stream');
 var fs = require('fs');
 var path = require('path');
 
-test('simple unpack', function() {
+test('simple unpack header', function() {
 	var stream = rpm_stream();
 	expect(1);
 
 	stop();
-	stream.on('header', function(header) {
-		equal(1, 1, "is good");
+	stream.on('header', function(chunk) {
+		ok(chunk.length >= 4, "is good");
 		start();
 	});
 
 	fs.createReadStream(path.join(__dirname,
 		'fixtures/mktemp-1.6-4mdv2010.1.i586.rpm')).pipe(stream);
+});
+
+test('fail unpack invalid header', function() {
+	var stream = rpm_stream();
+	expect(1);
+
+	stop();
+	stream.on('error', function(e) {
+		ok(e, "failed with " + e);
+		start();
+	});
+
+	fs.createReadStream(path.join(__dirname,
+		'extract.js')).pipe(stream);
 });
