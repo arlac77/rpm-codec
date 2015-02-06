987967e0929d6b3a1d91dfe3a9ac1e7199100760
Markus Felten <Markus.Felten@gmx.de>
2015-02-06 18:19:37 +0100

    moccaization...

--- test/extract.js
+++ test/extract.js
@@ -5,29 +5,31 @@ var assert = require('assert')
 var mocha = require('mocha')
 
 describe('simple unpack header', function() {
-	var stream = rpm_stream();
-	// TODO expect(1);
+  it('should work', function(done) {
+    var stream = rpm_stream();
+    // TODO expect(1);
 
-	// TODO stop();
-	stream.on('header', function(chunk) {
-		assert(chunk.length >= 4, "is good");
-		// TODO start();
-	});
+    stream.on('header', function(chunk) {
+      assert(chunk.length >= 4, "is good");
+      done();
+    });
 
-	fs.createReadStream(path.join(__dirname,
-		'fixtures/mktemp-1.6-4mdv2010.1.i586.rpm')).pipe(stream);
+    fs.createReadStream(path.join(__dirname,
+      'fixtures/mktemp-1.6-4mdv2010.1.i586.rpm')).pipe(stream);
+  });
 });
 
 describe('fail unpack invalid header', function() {
-	var stream = rpm_stream();
-	// TODO expect(1);
+  it('should work', function(done) {
+    var stream = rpm_stream();
+    // TODO expect(1);
 
-	// TODO stop();
-	stream.on('error', function(e) {
-		assert(e, "failed with " + e);
-		// TODO start();
-	});
+    stream.on('error', function(e) {
+      assert(e, "failed with " + e);
+      done();
+    });
 
-	fs.createReadStream(path.join(__dirname,
-		'extract.js')).pipe(stream);
+    fs.createReadStream(path.join(__dirname,
+      'extract.js')).pipe(stream);
+  });
 });
