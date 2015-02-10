644d4ee2c8f764d2d1bf31c68b3304660ee8edb1
Jochen Hinrichsen <jochen.hinrichsen@kambrium.net>
2015-02-10 17:36:53 +0100

    Adding cpio reading

--- test/header.js
+++ test/header.js
@@ -3,19 +3,22 @@
 "use strict";
 
 var fs = require('fs');
+var test = require('mocha');
 var path = require('path');
-// var Buffer = require('buffer').Buffer;
+var sbuff = require('simple-bufferstream')
+
+var cpio = require('cpio-stream');
 
 require('6to5/register');
 var assert = require('assert');
 var rpm = require('../');
 var header = require('../lib/header');
 
-describe('Convert byte[] to number', function() {
+test.describe('Convert byte[] to number', function() {
   assert.equal(0, header.num([0]), "Can convert 0");
 });
 
-describe('Convert byte[] to number', function() {
+test.describe('Convert byte[] to number', function() {
   assert.equal(65537, header.num([1, 0, 1]), "Can convert large numbers");
 });
 
@@ -24,8 +27,8 @@ Buffer.prototype.toByteArray = function() {
   return Array.prototype.slice.call(this, 0);
 };
 
-describe('Read header from rpm package', function() {
-  it('should work', function(done) {
+test.describe('Read header from rpm package', function() {
+  test.it('should work', function(done) {
     var filename = path.join(__dirname, 'fixtures/mktemp-1.6-4mdv2010.1.i586.rpm');
     fs.open(filename, 'r', function(status, fd) {
       if (status) {
@@ -44,6 +47,26 @@ describe('Read header from rpm package', function() {
     });
   });
 });
+
+var consume = function(buf) {
+  var extract = cpio.extract();
+
+  extract.on('entry', function(header, stream, callback) {
+    console.log('entry event');
+    stream.on('end', function() {
+      callback();
+    });
+
+    stream.resume(); // auto drain
+  });
+
+  extract.on('finish', function() {
+    console.log('finish event');
+    // all entries read
+  });
+
+  sbuff(buf).pipe(extract);
+};
 
 var parse = function(bs) {
   let hs = {};
@@ -90,6 +113,7 @@ var parse = function(bs) {
   let headerStoreSize = header.storeSize(ids);
   pos += headerStoreSize;
   console.log(`cpio payload starts at position ${pos} (${pos.toString(16)})`);
+  consume(new Buffer(bs.slice(pos)));
 
   console.log(`Complete header: ${JSON.stringify(hs)}`);
   return hs;
