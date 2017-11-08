71afb227240c9310d01a3e974ea98d46f29c8867
Markus Felten <Markus.Felten@gmx.de>
2017-11-08 23:35:15 +0100

    chore(rollup): make use of rollup-plugin-multi-entry (again)

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,67 +1,14 @@
-const external = ['ava'];
+import multiEntry from 'rollup-plugin-multi-entry';
 
-export default [
-  {
-    input: 'tests/util-test.js',
-    output: {
-      file: 'build/util-test.js',
-      format: 'cjs',
-      sourcemap: true
-    },
-    external
-  },
-  {
-    input: 'tests/field-test.js',
-    output: {
-      file: 'build/field-test.js',
-      format: 'cjs',
-      sourcemap: true
-    },
-    external
-  },
-  {
-    input: 'tests/encode-test.js',
-    output: {
-      file: 'build/encode-test.js',
-      format: 'cjs',
-      sourcemap: true
-    },
-    external
-  },
-  {
-    input: 'tests/decode-test.js',
-    output: {
-      file: 'build/decode-test.js',
-      format: 'cjs',
-      sourcemap: true
-    },
-    external
-  },
-  {
-    input: 'tests/lead-test.js',
-    output: {
-      file: 'build/lead-test.js',
-      format: 'cjs',
-      sourcemap: true
-    },
-    external
-  },
-  {
-    input: 'tests/header-test.js',
-    output: {
-      file: 'build/header-test.js',
-      format: 'cjs',
-      sourcemap: true
-    },
-    external
-  },
-  {
-    input: 'tests/types-test.js',
-    output: {
-      file: 'build/types-test.js',
-      format: 'cjs',
-      sourcemap: true
-    },
-    external
+export default {
+  input: 'tests/**/*-test.js',
+  external: ['ava'],
+
+  plugins: [multiEntry()],
+
+  output: {
+    file: 'build/bundle-test.js',
+    format: 'cjs',
+    sourcemap: true
   }
-];
+};
