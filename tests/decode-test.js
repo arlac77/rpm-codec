b030af329fe7b83fb416588ea51ee1a7440c65de
Markus Felten <Markus.Felten@gmx.de>
2018-09-20 00:10:13 +0200

    test: use ava@next throwsAsync()

--- tests/decode-test.js
+++ tests/decode-test.js
@@ -1,13 +1,13 @@
-import test from 'ava';
-import { RPMDecoder, contentDecoder } from '../src/codec';
-import { TYPE_STRING } from '../src/types';
+import test from "ava";
+import { RPMDecoder, contentDecoder } from "../src/codec";
+import { TYPE_STRING } from "../src/types";
 
-import { createReadStream } from 'fs';
-import { join } from 'path';
+import { createReadStream } from "fs";
+import { join } from "path";
 
-test('RPMDecoder lzma', async t => {
+test("RPMDecoder lzma", async t => {
   const input = createReadStream(
-    join(__dirname, '..', 'tests', 'fixtures', 'mktemp-1.6-4mdv2010.1.i586.rpm')
+    join(__dirname, "..", "tests", "fixtures", "mktemp-1.6-4mdv2010.1.i586.rpm")
   );
 
   const result = await RPMDecoder(input);
@@ -15,7 +15,7 @@ test('RPMDecoder lzma', async t => {
   console.log(result.signature.values);
 
   t.deepEqual(
-    result.signature.values.get('MD5'),
+    result.signature.values.get("MD5"),
     new Uint8Array([
       0x74,
       0x5c,
@@ -36,80 +36,83 @@ test('RPMDecoder lzma', async t => {
     ])
   );
 
-  t.is(result.header.values.get('PAYLOADCOMPRESSOR'), 'lzma');
+  t.is(result.header.values.get("PAYLOADCOMPRESSOR"), "lzma");
 
-  input.pipe(contentDecoder(result,(header, stream, callback) => {
+  input.pipe(
+    contentDecoder(result, (header, stream, callback) => {
       console.log(`extract: ${header.name}`);
-      stream.on('end', () => callback());
+      stream.on("end", () => callback());
       stream.resume();
-      }
-  ));
+    })
+  );
 });
 
-test('RPMDecoder gzip', async t => {
+test("RPMDecoder gzip", async t => {
   const input = createReadStream(
-    join(__dirname, '..', 'tests', 'fixtures', 'hello-2.3-1.el2.rf.i386.rpm')
+    join(__dirname, "..", "tests", "fixtures", "hello-2.3-1.el2.rf.i386.rpm")
   );
 
   const result = await RPMDecoder(input);
 
-  t.is(result.header.values.get('PAYLOADCOMPRESSOR'), 'gzip');
+  t.is(result.header.values.get("PAYLOADCOMPRESSOR"), "gzip");
 
   input.pipe(contentDecoder(result));
 });
 
 function collectEntries() {}
 
-test('RPMDecoder aarch64', async t => {
+test("RPMDecoder aarch64", async t => {
   const input = createReadStream(
     join(
       __dirname,
-      '..',
-      'tests',
-      'fixtures',
-      'filesystem-3.2-40.fc26.aarch64.rpm'
+      "..",
+      "tests",
+      "fixtures",
+      "filesystem-3.2-40.fc26.aarch64.rpm"
     )
   );
 
   const result = await RPMDecoder(input);
 
-  t.is(result.header.values.get('PAYLOADCOMPRESSOR'), 'xz');
+  t.is(result.header.values.get("PAYLOADCOMPRESSOR"), "xz");
 
   const files = new Set();
 
   const p = input.pipe(
     contentDecoder(result, (header, stream, callback) => {
       files.add(header.name);
-      stream.on('end', () => callback());
+      stream.on("end", () => callback());
       stream.resume();
     })
   );
 
   await new Promise((resolve, reject) => {
-    p.on('end', () => resolve());
-    p.on('error', err => reject(err));
+    p.on("end", () => resolve());
+    p.on("error", err => reject(err));
   });
 
-  t.true(files.has('./usr/src'));
+  t.true(files.has("./usr/src"));
 });
 
-test('fail RPMDecoder invalid header', async t => {
+test("fail RPMDecoder invalid header", async t => {
   const input = createReadStream(
-    join(__dirname, '..', 'tests', 'decode-test.js')
+    join(__dirname, "..", "tests", "decode-test.js")
   );
 
-  const error = await t.throws(RPMDecoder(input));
-  t.is(
-    error.message,
-    'Bad magic, this is not a lead. Expecting 237,171,238,219 but got 105,109,112,111'
-  );
+  await t.throwsAsync(async () => RPMDecoder(input), {
+    instanceOf: TypeError,
+    message:
+      "Bad magic, this is not a lead. Expecting 237,171,238,219 but got 105,109,112,111"
+  });
 });
 
-test('fail RPMDecoder short file', async t => {
+test("fail RPMDecoder short file", async t => {
   const input = createReadStream(
-    join(__dirname, '..', 'tests', 'fixtures', 'to-short.rpm')
+    join(__dirname, "..", "tests", "fixtures", "to-short.rpm")
   );
 
-  const error = await t.throws(RPMDecoder(input));
-  t.is(error.message, `Unexpected end of stream at 96 while reading signature`);
+  await t.throwsAsync(async () => RPMDecoder(input), {
+    instanceOf: TypeError,
+    message: `Unexpected end of stream at 96 while reading signature`
+  });
 });
